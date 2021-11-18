2521a661e5e483a8a89367103777ffc7527d81d6
唐堂 <1595691007@qq.com>
2021-11-18 22:53:00 +0800

    Initial commit

---
+++ Src/main.c
@@ -0,0 +1,266 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "i2c.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+#include<stdio.h>
+#include "AHT20-21_DEMO_V1_3.h" 
+//#include "AHT20-21_DEMO_V1_3.h" 
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+
+int fputc(int ch,FILE *f)
+ 
+{
+    HAL_UART_Transmit(&huart1,(uint8_t *)&ch,1,0xFFFF);    
+		//等待发送结束	
+		while(__HAL_UART_GET_FLAG(&huart1,UART_FLAG_TC)!=SET){
+		}		
+
+    return ch;
+}
+
+
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+	uint32_t CT_data[2]={0,0};
+	volatile int  c1,t1;
+	/***********************************************************************************/
+	/**///①刚上电，产品芯片内部就绪需要时间，延时100~500ms,建议500ms
+	/***********************************************************************************/
+	Delay_1ms(500);
+	/***********************************************************************************/
+	/**///②上电第一次发0x71读取状态字，判断状态字是否为0x18,如果不是0x18,进行寄存器初始化
+	/***********************************************************************************/
+
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  //MX_I2C1_Init();
+  MX_DMA_Init();
+  MX_USART1_UART_Init();
+	AHT20_Init();
+	Delay_1ms(500);
+  /* USER CODE BEGIN 2 */
+	if((AHT20_Read_Status()&0x18)!=0x18)
+	{
+		AHT20_Start_Init(); //重新初始化寄存器
+		Delay_1ms(10);
+	}
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  { 
+    /* USER CODE END WHILE */
+		AHT20_Read_CTdata(CT_data);       //不经过CRC校验，直接读取AHT20的温度和湿度数据    推荐每隔大于1S读一次
+    //AHT20_Read_CTdata_crc(CT_data);  //crc校验后，读取AHT20的温度和湿度数据 
+	
+
+		c1 = CT_data[0]*1000/1024/1024;  //计算得到湿度值c1（放大了10倍）
+		t1 = CT_data[1]*2000/1024/1024-500;//计算得到温度值t1（放大了10倍）
+		printf("正在检测");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		printf("\r\n");
+		HAL_Delay(1000);
+		printf("温度:%d%d.%d",t1/100,(t1/10)%10,t1%10);
+		printf("湿度:%d%d.%d",c1/100,(c1/10)%10,c1%10);
+		printf("\r\n");
+		printf("等待");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		HAL_Delay(100);
+		printf(".");
+		printf("\r\n");
+		HAL_Delay(1000);
+  /* USER CODE END 3 */
+	}
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
